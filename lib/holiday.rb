6caefd3eaa63690c4c7ad6d0d16d7fa1e86d6c8a
Kevin Mathias <komathias1@gmail.com>
2020-04-01 00:12:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 
-add_new_holiday_with_supplies(holiday_supplies, :fall, :succas, ["lulav", "esrog", "schach"])
 end
 
 def all_winter_holiday_supplies(holiday_hash)

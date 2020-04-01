b2def898b3d54a8439ebccad9a0d3a38cb1f2d4a
Kevin Mathias <komathias1@gmail.com>
2020-04-01 00:20:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | season, holiday|
      print "#{season.capitalize}:"
      holiday.each do |holiday, value|
-       print "  #{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{value.join(", ")}"
+       print "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{value.join(", ")}"
      end
   end 
 end
